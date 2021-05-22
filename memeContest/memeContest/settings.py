f90db1d027259f5132ed7732c37657f5a3fc3a72
chainedfarmsnetwork <chainedfarmsnetwork@gmail.com>
2021-05-22 02:03:39 +0200

    Add config files.

--- memeContest/memeContest/settings.py
+++ memeContest/memeContest/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d$44lx#d11jgp(%p687aqq*rd_hd0y64)3)#i53wx2v#osk6!('
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
